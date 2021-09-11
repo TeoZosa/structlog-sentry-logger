3b515e3e56d18775aca9041107823b28ad44e708
Teo Zosa <erinzosa@ucla.edu>
2021-09-10 19:16:10 -0700

    :bug: Fix compilation errors: `SystemError`, `TypeError`
    Errors due to `structlog_sentry` dependency.
    
    `TypeError` example:
    ```
    Run echo "Default output (JSON)"
      echo "Default output (JSON)"
      python ./docs_src/pure_structlog_logging_without_sentry.py
      echo "dev-local output (formatted)"
      export CI_ENVIRONMENT_SLUG=dev-local
      python ./docs_src/pure_structlog_logging_without_sentry.py
      shell: /usr/bin/bash --noprofile --norc -e -o pipefail {0}
      env:
        pythonLocation: /opt/hostedtoolcache/Python/3.7.11/x64
        LD_LIBRARY_PATH: /opt/hostedtoolcache/Python/3.7.11/x64/lib
    Default output (JSON)
    Traceback (most recent call last):
      File "./docs_src/pure_structlog_logging_without_sentry.py", line 3, in <module>
        LOGGER = structlog_sentry_logger.get_logger()
      File "structlog_sentry_logger/_config.py", line 80, in get_logger
        set_structlog_config(timestamper)
      File "structlog_sentry_logger/_config.py", line 204, in set_structlog_config
        SentryBreadcrumbJsonProcessor(level=logging.ERROR, tag_keys="__all__"),
      File "structlog_sentry_logger/_config.py", line 406, in __init__
        super().__init__(
      File "structlog_sentry_logger/_config.py", line 352, in __init__
        super().__init__(*args, **kwargs)
    TypeError: int object expected; got tuple[
    Error: Process completed with exit code 1.
    ```
    
    `SystemError` example:
    ```
    Run echo "Default output (JSON)"
    Default output (JSON)
    Traceback (most recent call last):
      File "/home/runner/work/structlog-sentry-logger/structlog-sentry-logger/./docs_src/pure_structlog_logging_without_sentry.py", line 3, in <module>
        LOGGER = structlog_sentry_logger.get_logger()
      File "structlog_sentry_logger/_config.py", line 77, in get_logger
        set_structlog_config(timestamper)
      File "structlog_sentry_logger/_config.py", line 201, in set_structlog_config
        SentryBreadcrumbJsonProcessor(level=logging.ERROR, tag_keys="__all__"),
      File "structlog_sentry_logger/_config.py", line 242, in __init__
        super().__init__(
      File "/opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/structlog_sentry/__init__.py", line 132, in __init__
        super().__init__(*args, **kwargs)
      File "/opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/structlog_sentry/__init__.py", line 33, in __init__
        self.level = level
    SystemError: Objects/dictobject.c:bug: bad argument to internal function
    Error: Process completed with exit code 1.
    ```

--- build.py
+++ build.py
@@ -12,6 +12,7 @@ def build(setup_kwargs: dict) -> None:
                     "--disallow-untyped-defs",
                     "structlog_sentry_logger/__init__.py",
                     "structlog_sentry_logger/_config.py",
+                    "structlog_sentry_logger/structlog_sentry.py",
                 ]
             ),
         }
