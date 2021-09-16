bbf4115cb338c3822560c104589eabf39bb20056
github-action-benchmark <github@users.noreply.github.com>
2021-09-16 01:27:47 +0000

    add Benchmark (pytest) benchmark result for 4cc61459ec5c5a6f91481c6ff85a23300f099946

--- dev/bench/ubuntu-latest/CPython3.7/c_library/data.js
+++ dev/bench/ubuntu-latest/CPython3.7/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631754487827,
+  "lastUpdate": 1631755667388,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -34,6 +34,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0011154100359363774",
             "extra": "mean: 45.38193766666646 msec\nrounds: 24"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "4cc61459ec5c5a6f91481c6ff85a23300f099946",
+          "message": ":construction_worker: Benchmark based on process time\n\nGiven that benchmarks run automatically on cloud instances during CI,\nresource contention may confound metrics. Benchmarking based on process\ntime will lead to more accurate results within and between environment.\n\nNote: logging is inherently I/O-bound and process time, by definition,\nexcludes time spent waiting on I/O, so these benchmarks are *not*\na good indication of real-world performance. Instead, they are meant to\ncompare performance across different combinations of Python\ninterpreters, platforms, and library types (viz. Python and C).",
+          "timestamp": "2021-09-16T01:00:37Z",
+          "tree_id": "addd2c89830236692cba95e762cb2fb0018b4598"
+        },
+        "date": 1631755666645,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 103.5763603859048,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00030100058175767025",
+            "extra": "mean: 9.654712680327828 msec\nrounds: 122"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 21.142138471049066,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007379748729367263",
+            "extra": "mean: 47.29890504545448 msec\nrounds: 22"
           }
         ]
       }
