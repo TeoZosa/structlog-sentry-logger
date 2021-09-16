f4fe3144df0c352b444d1d618cf2b434d99651ef
github-action-benchmark <github@users.noreply.github.com>
2021-09-16 19:35:22 +0000

    add Benchmark (pytest) benchmark result for 869f972663228706bd822727098de0dd4cfcebeb

---
+++ dev/bench/Windows/CPython/3.7/64bit/c_library/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1631820922049,
+  "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "distinct": true,
+          "id": "869f972663228706bd822727098de0dd4cfcebeb",
+          "message": ":alembic: Test setting env implicitly via `TOXENV`",
+          "timestamp": "2021-09-16T12:23:08-07:00",
+          "tree_id": "1678e07a73b71eae747346faf8b23adb273c6396",
+          "url": "https://github.com/TeoZosa/structlog-sentry-logger/commit/869f972663228706bd822727098de0dd4cfcebeb"
+        },
+        "date": 1631820918787,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 54.857142857142854,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0012757759076995714",
+            "extra": "mean: 18.229166666666668 msec\nrounds: 6"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 10.520547945205479,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00913316558164072",
+            "extra": "mean: 95.05208333333333 msec\nrounds: 6"
+          }
+        ]
+      }
+    ]
+  }
+}
