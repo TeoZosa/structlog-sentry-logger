35bb49f6243415d4cc41df73aeb913004f53b9a3
github-action-benchmark <github@users.noreply.github.com>
2021-09-16 01:10:35 +0000

    add Benchmark (pytest) benchmark result for a830bc4a08c52c80a7d23252aaa468971a09cae7

---
+++ dev/bench/ubuntu-latest/CPython3.8/pure_python/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1631754635957,
+  "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "a830bc4a08c52c80a7d23252aaa468971a09cae7",
+          "message": ":memo: Remove year from \"Performance\" section\n\nEasier to maintain; preempt risk of looking dated if the year\naccidentally doesn't get updated.",
+          "timestamp": "2021-09-16T00:44:59Z",
+          "tree_id": "6e6f16f6f92bbe32b37814f19654a748ccb5504f"
+        },
+        "date": 1631754634778,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 72.89912236550302,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0011928116397602203",
+            "extra": "mean: 13.7175862692308 msec\nrounds: 78"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 18.8336516213849,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005561974095714543",
+            "extra": "mean: 53.0964477894737 msec\nrounds: 19"
+          }
+        ]
+      }
+    ]
+  }
+}
