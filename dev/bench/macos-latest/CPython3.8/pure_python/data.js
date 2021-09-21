671c06828a98d0dac4179327146936b1ff3e948b
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 05:38:04 +0000

    add Benchmark (pytest) benchmark result for 77eb8a129cd88614cb31863ea67655674f31b9d0

--- dev/bench/macos-latest/CPython3.8/pure_python/data.js
+++ dev/bench/macos-latest/CPython3.8/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632186671038,
+  "lastUpdate": 1632202684137,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -680,6 +680,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0030696277066028983",
             "extra": "mean: 70.06659999999988 msec\nrounds: 15"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "77eb8a129cd88614cb31863ea67655674f31b9d0",
+          "message": ":alembic: Test fixing erroneous benchmarks publishing",
+          "timestamp": "2021-09-21T05:32:34Z",
+          "tree_id": "1d2648c09d2e26a853668b7a3c5c060cabb2364c"
+        },
+        "date": 1632202682173,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 72.67238728792191,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0006691071047913107",
+            "extra": "mean: 13.760384615384709 msec\nrounds: 78"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 14.119361315397285,
+            "unit": "iter/sec",
+            "range": "stddev: 0.001988545967953945",
+            "extra": "mean: 70.82473333333367 msec\nrounds: 15"
           }
         ]
       }
