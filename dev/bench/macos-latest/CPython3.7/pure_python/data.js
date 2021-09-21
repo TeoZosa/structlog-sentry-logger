414b063fd3d49e3f6bc35c357485433f6a439c13
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 19:05:30 +0000

    add Benchmark (pytest) benchmark result for bc817a8e82da08d7c19b19f4074f1b24355230d3

--- dev/bench/macos-latest/CPython3.7/pure_python/data.js
+++ dev/bench/macos-latest/CPython3.7/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632250747778,
+  "lastUpdate": 1632251130143,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -850,6 +850,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0009206602341072828",
             "extra": "mean: 71.67446666666676 msec\nrounds: 15"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "bc817a8e82da08d7c19b19f4074f1b24355230d3",
+          "message": ":bug: Fix `test` target help text rendering",
+          "timestamp": "2021-09-21T19:02:06Z",
+          "tree_id": "af626cf566f0baa9ca47e7f272fa9c0f69d41689"
+        },
+        "date": 1632251128185,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 80.3580909596192,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002630300170584792",
+            "extra": "mean: 12.444297619047603 msec\nrounds: 84"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 13.238595422850224,
+            "unit": "iter/sec",
+            "range": "stddev: 0.001243250604638828",
+            "extra": "mean: 75.53671428571411 msec\nrounds: 14"
           }
         ]
       }
