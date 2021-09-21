8d8162f1c26fa931b1b7cfd549fde8be07ebc99c
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 21:01:51 +0000

    add Benchmark (pytest) benchmark result for a208950b450fd4bf11b08fcb9cd8671f83ca42d5

--- dev/bench/Darwin/Cpython3.7/pure_python/data.js
+++ dev/bench/Darwin/Cpython3.7/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631820614306,
+  "lastUpdate": 1632258111723,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0012937217905825648",
             "extra": "mean: 72.88392857142854 msec\nrounds: 14"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "id": "a208950b450fd4bf11b08fcb9cd8671f83ca42d5",
+          "message": ":construction_worker: Invoke `tox-%` target directly when benchmarking in CI",
+          "timestamp": "2021-09-21T20:55:29Z",
+          "tree_id": "1c37749a5fb85d6155bbe2f0ae768542dc655be8"
+        },
+        "date": 1632258109814,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 67.55162378372376,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0006156840171550692",
+            "extra": "mean: 14.803493150684934 msec\nrounds: 73"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 12.110902327435921,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0016604544133984993",
+            "extra": "mean: 82.57023076923093 msec\nrounds: 13"
           }
         ]
       }
