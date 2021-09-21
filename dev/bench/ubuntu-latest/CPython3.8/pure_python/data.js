2c2a2359b305fbedf8cf250bcfc898fccf128daa
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 20:41:34 +0000

    add Benchmark (pytest) benchmark result for a4559f8431061ad1283159a520d28aaaa29cf6e8

--- dev/bench/ubuntu-latest/CPython3.8/pure_python/data.js
+++ dev/bench/ubuntu-latest/CPython3.8/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632256592486,
+  "lastUpdate": 1632256894157,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -1020,6 +1020,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00011676586252229774",
             "extra": "mean: 40.6558839600001 msec\nrounds: 25"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "a4559f8431061ad1283159a520d28aaaa29cf6e8",
+          "message": ":construction_worker: Run CI mutation testing on MacOS runner\n\nSince MacOS is the default developer OS, ensure maximal `.mutmut`\ncache-consistency in order to shorten mutation testing job times in CI.",
+          "timestamp": "2021-09-21T20:37:13Z",
+          "tree_id": "60fb4d43a0269d983444569658694f1610d0734d"
+        },
+        "date": 1632256892892,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 73.8777935001423,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009163943520577702",
+            "extra": "mean: 13.535867174999938 msec\nrounds: 80"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 18.647001225904297,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0029968537383640854",
+            "extra": "mean: 53.62792589999974 msec\nrounds: 20"
           }
         ]
       }
