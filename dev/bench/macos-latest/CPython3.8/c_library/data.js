2e88bd5ce6ea299e90ae11d6d16f22e5cce7fbc3
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 18:59:23 +0000

    add Benchmark (pytest) benchmark result for 9bce65619929b63ece96b7e408ebafc8ec14ae85

--- dev/bench/macos-latest/CPython3.8/c_library/data.js
+++ dev/bench/macos-latest/CPython3.8/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632233573704,
+  "lastUpdate": 1632250763018,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -816,6 +816,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0009183996831565933",
             "extra": "mean: 72.58714285714296 msec\nrounds: 14"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "9bce65619929b63ece96b7e408ebafc8ec14ae85",
+          "message": ":bug: Preempt premature brace expansion by the shell\n\nIf multiple commands are passed via brace syntax, quoting the string\nallows direct evaluation by tox as opposed to generating multiple envs\nin the call to tox.\n\nFor example,\n`poetry run tox -e py3{8,9} -- $(POSARGS)`\nbecomes\n`poetry run tox -e py38 py39 -- $(POSARGS)`\nWhereas\n`poetry run tox -e \"py3{8,9}\" -- $(POSARGS)`\npasses \"py3{8,9}\" directly to tox.\nNote: to activate this behavior via the `tox-%` make target, users would\n have to also invoke it with quotes, e.g., `make tox-\"py3{8,9}\"`.\n- If a user were to enter `make tox-py3{8,9}`, this would become\n`make tox-py38 tox-py39\"`, which, while this would still work, leads to\n different semantics as separate make targets are actually invoked.\n\nsee: https://www.gnu.org/software/bash/manual/html_node/Brace-Expansion.html",
+          "timestamp": "2021-09-21T18:52:19Z",
+          "tree_id": "648410f6afef021ae2b6552a61636df5ec976b9b"
+        },
+        "date": 1632250757731,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 71.0957798653059,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0010643391399610498",
+            "extra": "mean: 14.06553246753245 msec\nrounds: 77"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 12.609431855027596,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0130685408309631",
+            "extra": "mean: 79.30571428571406 msec\nrounds: 14"
           }
         ]
       }
