3e26fcbca7d2c54fae90fb3a7343dbaf6ea00e07
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 19:51:56 +0000

    add Benchmark (pytest) benchmark result for 4222aae8292928efcdb5e62004dd68604a3a219c

--- dev/bench/macos-latest/CPython3.9/pure_python/data.js
+++ dev/bench/macos-latest/CPython3.9/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632253426173,
+  "lastUpdate": 1632253916005,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -952,6 +952,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0018128963356493068",
             "extra": "mean: 71.2141333333335 msec\nrounds: 15"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "4222aae8292928efcdb5e62004dd68604a3a219c",
+          "message": ":memo: Document \"*.so\" and \"*.pyd\" cleanup rationale",
+          "timestamp": "2021-09-21T19:35:10Z",
+          "tree_id": "4cd730a1f49dbf9d89a30be6087da32f6940f0d5"
+        },
+        "date": 1632253914903,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 76.9163467427367,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00039816035010884913",
+            "extra": "mean: 13.001137500000038 msec\nrounds: 80"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 13.872845200319253,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0030651636979510777",
+            "extra": "mean: 72.0832666666667 msec\nrounds: 15"
           }
         ]
       }
