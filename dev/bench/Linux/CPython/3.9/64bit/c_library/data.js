4e7e8366c6fe5ad6367de0de83bf90526fdea026
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 21:13:56 +0000

    add Benchmark (pytest) benchmark result for a208950b450fd4bf11b08fcb9cd8671f83ca42d5

---
+++ dev/bench/Linux/CPython/3.9/64bit/c_library/data.js
@@ -0,0 +1,76 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632258836819,
+  "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "id": "a208950b450fd4bf11b08fcb9cd8671f83ca42d5",
+          "message": ":construction_worker: Invoke `tox-%` target directly when benchmarking in CI",
+          "timestamp": "2021-09-21T20:55:29Z",
+          "tree_id": "1c37749a5fb85d6155bbe2f0ae768542dc655be8"
+        },
+        "date": 1632258834464,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 75.3344077544503,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008978685401913809",
+            "extra": "mean: 13.274146964285734 msec\nrounds: 84"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 19.77562476323809,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0017060439620432556",
+            "extra": "mean: 50.567302523809545 msec\nrounds: 21"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "id": "a208950b450fd4bf11b08fcb9cd8671f83ca42d5",
+          "message": ":construction_worker: Invoke `tox-%` target directly when benchmarking in CI",
+          "timestamp": "2021-09-21T20:55:29Z",
+          "tree_id": "1c37749a5fb85d6155bbe2f0ae768542dc655be8"
+        },
+        "date": 1632258834464,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 75.3344077544503,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008978685401913809",
+            "extra": "mean: 13.274146964285734 msec\nrounds: 84"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 19.77562476323809,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0017060439620432556",
+            "extra": "mean: 50.567302523809545 msec\nrounds: 21"
+          }
+        ]
+      }
+    ]
+  }
+}
