f4a48f0919fd7ec8344aab2b537aa4d30e73515d
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 19:43:46 +0000

    add Benchmark (pytest) benchmark result for 84d84fd9451cd9f82ff8372975b0a7eaedf96fe5

--- dev/bench/macos-latest/CPython3.9/pure_python/data.js
+++ dev/bench/macos-latest/CPython3.9/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632252608585,
+  "lastUpdate": 1632253426173,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -918,6 +918,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0021034212425973314",
             "extra": "mean: 71.4410666666667 msec\nrounds: 15"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "84d84fd9451cd9f82ff8372975b0a7eaedf96fe5",
+          "message": "Update help text: `test-mutations`",
+          "timestamp": "2021-09-21T19:31:16Z",
+          "tree_id": "4cbb2406183f46e03463e8f10e4a00a0ef2d67ae"
+        },
+        "date": 1632253423381,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 69.46587774533101,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0010423056511559647",
+            "extra": "mean: 14.395556962025324 msec\nrounds: 79"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 14.042156425877975,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0018128963356493068",
+            "extra": "mean: 71.2141333333335 msec\nrounds: 15"
           }
         ]
       }
