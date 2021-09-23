299be0fd5e7b877a8ecd1968f20d0088d18514d9
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 21:38:55 +0000

    add Benchmark (pytest) benchmark result for acb1ab34f25829565d1ee774a670a5ca76be55fb

--- dev/bench/Darwin/Cpython3.8/pure_python/data.js
+++ dev/bench/Darwin/Cpython3.8/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632404859934,
+  "lastUpdate": 1632433135232,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -306,6 +306,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0018305771533277057",
             "extra": "mean: 69.64199999999985 msec\nrounds: 15"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "acb1ab34f25829565d1ee774a670a5ca76be55fb",
+          "message": ":memo: Fix erroneous tox `c_library` testenv description",
+          "timestamp": "2021-09-23T21:33:28Z",
+          "tree_id": "6b95a17c32e23290ac799793ec932c69bade4627"
+        },
+        "date": 1632433133743,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 74.22031558478228,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007844308845642311",
+            "extra": "mean: 13.473399999999923 msec\nrounds: 80"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 13.950166285982146,
+            "unit": "iter/sec",
+            "range": "stddev: 0.001398213884449271",
+            "extra": "mean: 71.68373333333325 msec\nrounds: 15"
           }
         ]
       }
