9f055aa2ca5366967d51282475a8f2a373555521
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 22:59:19 +0000

    add Benchmark (pytest) benchmark result for dff1097d17f9c422e14df0fa1a85de7646b86a2c

--- dev/bench/Linux/CPython/3.9/64bit/pure_python/data.js
+++ dev/bench/Linux/CPython/3.9/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632432984036,
+  "lastUpdate": 1632437959566,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -340,6 +340,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.004830597474655418",
             "extra": "mean: 54.82796528571433 msec\nrounds: 21"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "dff1097d17f9c422e14df0fa1a85de7646b86a2c",
+          "message": ":rotating_light: Fix `pylint` error: `duplicate-code`\n\n```\npylint...............................................................................Failed\n- hook id: pylint\n- duration: 11.06s\n- exit code: 8\n\n************* Module tests.structlog_sentry_logger.child_module_2\ntests/structlog_sentry_logger/child_module_2.py:1:0: R0801: Similar lines in 2 files\n==structlog_sentry_logger._config:[242:247]\n==structlog_sentry_logger.structlog_sentry:[151:156]\n        level: int = logging.WARNING,\n        active: bool = True,\n        as_extra: bool = True,\n        tag_keys: Union[List[str], str] = None,\n    ) -> None: (duplicate-code)\n\n-----------------------------------\nYour code has been rated at 9.96/10\n```",
+          "timestamp": "2021-09-23T22:55:20Z",
+          "tree_id": "b91ef84a8a60ed6c6aaf5fdcc88910d0c1793774"
+        },
+        "date": 1632437958331,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 77.91046776208968,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000681261738332748",
+            "extra": "mean: 12.835245747126528 msec\nrounds: 87"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 18.35171617751712,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0038343775544623835",
+            "extra": "mean: 54.49081657142837 msec\nrounds: 21"
           }
         ]
       }
