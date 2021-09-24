c17fabd1884c6195f3e88aca7b7940288a1535a5
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 18:26:52 +0000

    add Benchmark (pytest) benchmark result for 59bec1743c18c5ddcd86b68ff9615b19467ee85a

--- dev/bench/Linux/CPython/3.8/64bit/c_library/data.js
+++ dev/bench/Linux/CPython/3.8/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632495224040,
+  "lastUpdate": 1632508012518,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -476,6 +476,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00371394434648941",
             "extra": "mean: 52.80716080952391 msec\nrounds: 21"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "59bec1743c18c5ddcd86b68ff9615b19467ee85a",
+          "message": "Merge pull request #285 from TeoZosa/add-cloud-logging-compatibility-support\n\n✨ Add Cloud Logging Compatibility Support",
+          "timestamp": "2021-09-24T18:25:20Z",
+          "tree_id": "542f07a9b21af752a2a87f10c518e7496caf8d9d"
+        },
+        "date": 1632508011229,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 71.4456571471523,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0010452128225779157",
+            "extra": "mean: 13.99665199999995 msec\nrounds: 79"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 18.17352342225776,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0025197589541581986",
+            "extra": "mean: 55.0251031000001 msec\nrounds: 20"
           }
         ]
       }
