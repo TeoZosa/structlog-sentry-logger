e6eef8d5d85c1fae98500fa36a1b808d6c9c7486
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 20:27:55 +0000

    add Benchmark (pytest) benchmark result for 5cb1945063e7cbb980063c7ac36de4034f2aafaa

--- dev/bench/Darwin/Cpython3.7/pure_python/data.js
+++ dev/bench/Darwin/Cpython3.7/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632510121155,
+  "lastUpdate": 1632515275147,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -544,6 +544,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0016982835047885919",
             "extra": "mean: 72.78264285714295 msec\nrounds: 14"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "5cb1945063e7cbb980063c7ac36de4034f2aafaa",
+          "message": "Merge pull request #289 from TeoZosa/namespace-local-dev-formatting-option-flag-user-and-simplify-workflow\n\n✨ Rename Local Development Formatting Option Flag and Simplify Workflow",
+          "timestamp": "2021-09-24T20:21:39Z",
+          "tree_id": "2f92d8bab16aebf909dd690e70ce414297a713a1"
+        },
+        "date": 1632515274217,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 68.13301017089556,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0013456768855711673",
+            "extra": "mean: 14.677173333333377 msec\nrounds: 75"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 12.595800753810282,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0030839529453660116",
+            "extra": "mean: 79.39153846153813 msec\nrounds: 13"
           }
         ]
       }
