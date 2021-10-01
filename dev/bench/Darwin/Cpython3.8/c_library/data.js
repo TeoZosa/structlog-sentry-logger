965fd6988c97b331f7067cf357c9bf218c5fb658
github-action-benchmark <github@users.noreply.github.com>
2021-10-01 03:47:30 +0000

    add Benchmark (pytest) benchmark result for a5ec72f567f51305b8136c6f0dffe7ae454762cd

--- dev/bench/Darwin/Cpython3.8/c_library/data.js
+++ dev/bench/Darwin/Cpython3.8/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633008516564,
+  "lastUpdate": 1633060049990,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -782,6 +782,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0024550340589834115",
             "extra": "mean: 81.0106923076927 msec\nrounds: 13"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "a5ec72f567f51305b8136c6f0dffe7ae454762cd",
+          "message": ":coffin: Remove redundant SKIP flag\n\nExit code ignored in `pre-commit` config.",
+          "timestamp": "2021-10-01T03:41:06Z",
+          "tree_id": "80e658be0e3340acb740c077c9d1b7fc265e636b"
+        },
+        "date": 1633060048558,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 67.09927057501278,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008194329959546093",
+            "extra": "mean: 14.90329166666667 msec\nrounds: 72"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 12.072494400219886,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0014737935439732208",
+            "extra": "mean: 82.83292307692321 msec\nrounds: 13"
           }
         ]
       }
