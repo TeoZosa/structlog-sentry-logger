7bf09729a60945b378adab3cfefdcbbae0dd1ebc
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 23:23:03 +0000

    add Benchmark (pytest) benchmark result for 41985f89d3390fb7022861f7554ab4f1d631c006

--- dev/bench/Darwin/Cpython3.8/pure_python/data.js
+++ dev/bench/Darwin/Cpython3.8/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633821182875,
+  "lastUpdate": 1633821782992,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -1163,6 +1163,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0062501551867910725",
             "extra": "mean: 87.189230769231 msec\nrounds: 13"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "41985f89d3390fb7022861f7554ab4f1d631c006",
+          "message": ":bookmark: Bump version number to `0.12.0`",
+          "timestamp": "2021-10-09T23:16:59Z",
+          "tree_id": "3a04d4d7745b241fd1f62babfc4ebfcd4295067c"
+        },
+        "date": 1633821779583,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 73.36198474813644,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00047481403763398073",
+            "extra": "mean: 13.631037974683506 msec\nrounds: 79"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 70.08473882057434,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004641345793236024",
+            "extra": "mean: 14.26844155844148 msec\nrounds: 77"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 11.507557108649543,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0020191090500659837",
+            "extra": "mean: 86.89941666666678 msec\nrounds: 12"
           }
         ]
       }
