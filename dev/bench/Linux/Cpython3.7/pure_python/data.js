a37ca44f125c441119befa66937d50d81a05c9b5
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 21:10:12 +0000

    add Benchmark (pytest) benchmark result for e2b620812dcfdbcf5ff0a31141998660e8ae54e5

--- dev/bench/Linux/Cpython3.7/pure_python/data.js
+++ dev/bench/Linux/Cpython3.7/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633632204175,
+  "lastUpdate": 1633813812184,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -102,6 +102,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00005235845438948042",
             "extra": "mean: 45.6539776363635 msec\nrounds: 22"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "e2b620812dcfdbcf5ff0a31141998660e8ae54e5",
+          "message": ":green_heart: Allow spurious interrogate failures\n\nThis hook works fine locally, but recent runs throw spurious error.\n\nExample error log:\n```\n\ninterrogate..........................................................................Failed\n- hook id: interrogate\n- duration: 0.24s\n- exit code: 2\n\nUsage: interrogate [OPTIONS] [PATHS]...\n\nError: Invalid value for '[PATHS]...': Path 'tests/structlog_sentry_logger/child_module_1.py' does not exist.\n```",
+          "timestamp": "2021-10-09T21:08:10Z",
+          "tree_id": "44c0682dacb9aed51a382f8e2099e69f28999f8d"
+        },
+        "date": 1633813810949,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 115.93900234355988,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00015073957719583206",
+            "extra": "mean: 8.625225159663861 msec\nrounds: 119"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 24.6735365911775,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00025584201000963174",
+            "extra": "mean: 40.529252719999995 msec\nrounds: 25"
           }
         ]
       }
