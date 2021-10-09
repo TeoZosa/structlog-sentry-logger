8dda78f64dea0799aa0682aaf6033b55275cbb81
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 21:44:58 +0000

    add Benchmark (pytest) benchmark result for b73fc2c6870cb9d950061342359b6a6bc7818919

--- dev/bench/Darwin/Cpython3.8/pure_python/data.js
+++ dev/bench/Darwin/Cpython3.8/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633814538478,
+  "lastUpdate": 1633815898249,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -1088,6 +1088,40 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.004124842133802004",
             "extra": "mean: 90.67258333333344 msec\nrounds: 12"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "b73fc2c6870cb9d950061342359b6a6bc7818919",
+          "message": "Merge pull request #303 from TeoZosa/add-non-str-key-serialization-functionality\n\n✨  Enable Non-String Key JSON Serialization",
+          "timestamp": "2021-10-09T21:36:31Z",
+          "tree_id": "1d60d5d7b2cd69e4bba934a6a28c39159fefba23"
+        },
+        "date": 1633815896801,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 65.48335813212383,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0010955500629967678",
+            "extra": "mean: 15.271055555555499 msec\nrounds: 72"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 12.647267703742626,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0013600943971764624",
+            "extra": "mean: 79.06846153846149 msec\nrounds: 13"
           }
         ]
       }
