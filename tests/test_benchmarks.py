017dc45a305b120ea5bb0186fff57e79084281d5
Teo Zosa <erinzosa@ucla.edu>
2021-10-09 15:37:49 -0700

    :recycle: Rename and maximally leverage logging helper method

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -7,7 +7,7 @@ import structlog_sentry_logger
 from tests.structlog_sentry_logger import test__config
 
 
-def _basic_logging_helper_method(benchmark: BenchmarkFixture, test_cases: dict) -> None:
+def _benchmark_runner(benchmark: BenchmarkFixture, test_cases: dict) -> None:
     logger = structlog_sentry_logger.get_logger()
     benchmark(lots_of_logging, logger=logger, test_cases=test_cases)
 
@@ -25,14 +25,12 @@ def lots_of_logging(logger: Any, test_cases: dict) -> None:
 
 
 def test_logging_orjson_serializer(benchmark: BenchmarkFixture) -> None:
-    test_cases = test__config.TestBasicLogging.test_cases
-    _basic_logging_helper_method(benchmark, test_cases=test_cases)
+    _benchmark_runner(benchmark, test_cases=test__config.TestBasicLogging.test_cases)
 
 
 @pytest.mark.usefixtures("temporarily_set_stlib_json_as_default_serializer")
 def test_logging_stdlib_json_serializer(benchmark: BenchmarkFixture) -> None:
-    test_cases = test__config.TestBasicLogging.test_cases
-    _basic_logging_helper_method(benchmark, test_cases=test_cases)
+    _benchmark_runner(benchmark, test_cases=test__config.TestBasicLogging.test_cases)
 
 
 @pytest.fixture(scope="function")
@@ -52,10 +50,8 @@ def temporarily_set_stlib_json_as_default_serializer() -> Generator:
 
 
 def test_logging_orjson_serializer_non_str_keys(benchmark: BenchmarkFixture) -> None:
-    logger = structlog_sentry_logger.get_logger()
-    benchmark(
-        lots_of_logging,
-        logger=logger,
+    _benchmark_runner(
+        benchmark,
         test_cases={
             "dummy kwarg for dict unpacking in log function": test__config.TestBasicLoggingNonStringKeys.test_cases
         },
