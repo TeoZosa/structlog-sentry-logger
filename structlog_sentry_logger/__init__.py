249d544d88aa5642c847ecd791d4a0f14e7d9572
Teo Zosa <erinzosa@ucla.edu>
2021-10-13 14:53:19 -0700

    :sparkles: Only inject defined `.env` values expected by the library
    And only if they are not already present in the environment. For
    example, they may be overridden at runtime by users, by a deployment
    system, etc.

--- structlog_sentry_logger/__init__.py
+++ structlog_sentry_logger/__init__.py
@@ -1,9 +1,9 @@
 """Structlog Sentry Logger"""
-
+import os
 from typing import List
 
 import sentry_sdk
-from dotenv import find_dotenv, load_dotenv
+from dotenv import dotenv_values, find_dotenv
 
 from structlog_sentry_logger._config import (
     _ENV_VARS_REQUIRED_BY_LIBRARY,
@@ -20,6 +20,21 @@ __all__: List[str] = [
     "getLogger",
 ]
 
-load_dotenv(find_dotenv())
+# Inject into the environment ONLY the env vars required by the library;
+# we manually update/add to the the environment ONLY the keys in a user's `.env` for
+# which the library is inspecting (i.e., the set intersection between the
+# aforementioned), and only if they weren't already defined in the environment.
+users_dotenv_values = dotenv_values(find_dotenv())
+legal_env_vars_keys = (
+    _ENV_VARS_REQUIRED_BY_LIBRARY.values() & users_dotenv_values.keys()
+)
+
+for k in legal_env_vars_keys:
+    v = users_dotenv_values[k]
+    # Any env-var-to-add already defined in the environment will take precedent over
+    # what is defined in a user's `.env` file.
+    if k not in os.environ and v is not None:
+        os.environ[k] = v
+
 # Note: if DSN isn't defined, will silently not transmit telemetry
 sentry_sdk.init()  # pylint: disable=abstract-class-instantiated
