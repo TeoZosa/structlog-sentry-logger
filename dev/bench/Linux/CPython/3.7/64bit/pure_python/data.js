ea90942dc3c30705367544479268310454dde34c
github-action-benchmark <github@users.noreply.github.com>
2021-10-13 03:43:07 +0000

    add Benchmark (pytest) benchmark result for 517775d778bf4dff722d354f4af11cd4ddbef359

--- dev/bench/Linux/CPython/3.7/64bit/pure_python/data.js
+++ dev/bench/Linux/CPython/3.7/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634096232945,
+  "lastUpdate": 1634096587382,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -1143,6 +1143,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.002961905582299801",
             "extra": "mean: 60.05847277777784 msec\nrounds: 18"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "517775d778bf4dff722d354f4af11cd4ddbef359",
+          "message": "Merge pull request #307 from TeoZosa/dependabot/pip/dot-github/workflows/pip-21.3\n\n⬆️ Bump pip from 21.2.4 to 21.3 in /.github/workflows",
+          "timestamp": "2021-10-13T03:36:04Z",
+          "tree_id": "d4f8eecaa51b5570a5891ed3989da8e6910af976"
+        },
+        "date": 1634096586168,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 80.00103724011514,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0006803829303177173",
+            "extra": "mean: 12.499837933333286 msec\nrounds: 90"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 72.88261511594877,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0015406843344282642",
+            "extra": "mean: 13.720693177777754 msec\nrounds: 90"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 16.825637066383106,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0030364781179102276",
+            "extra": "mean: 59.43311365000002 msec\nrounds: 20"
           }
         ]
       }
