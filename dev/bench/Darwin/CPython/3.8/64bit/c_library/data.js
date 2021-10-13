aaff82a4fb1fdf47e389130e61e9fde07c272a89
github-action-benchmark <github@users.noreply.github.com>
2021-10-13 03:20:17 +0000

    add Benchmark (pytest) benchmark result for d4a9777225fd53e83e0d9cf83d19c066ef6ef752

--- dev/bench/Darwin/CPython/3.8/64bit/c_library/data.js
+++ dev/bench/Darwin/CPython/3.8/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633838802571,
+  "lastUpdate": 1634095217845,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0016588749779016347",
             "extra": "mean: 81.22276923076927 msec\nrounds: 13"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "d4a9777225fd53e83e0d9cf83d19c066ef6ef752",
+          "message": ":arrow_up: Update `poetry.lock` lockfile",
+          "timestamp": "2021-10-13T03:08:47Z",
+          "tree_id": "3fa70fd16c4aea88e8acde24146228afb636d9e8"
+        },
+        "date": 1634095216282,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 66.96281120740274,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0014895935023053278",
+            "extra": "mean: 14.933662162162184 msec\nrounds: 74"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 66.69263233152076,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008762413131773995",
+            "extra": "mean: 14.994160000000072 msec\nrounds: 75"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 10.568878703621346,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0063960212129151916",
+            "extra": "mean: 94.6174166666666 msec\nrounds: 12"
           }
         ]
       }
