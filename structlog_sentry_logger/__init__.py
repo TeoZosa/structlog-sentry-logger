baf1d766cf4fb1e7580659126f702fc1d412e52d
Teo Zosa <erinzosa@ucla.edu>
2021-10-13 15:14:54 -0700

    :recycle: Extract `__init__` setup logic into standalone functions

--- structlog_sentry_logger/__init__.py
+++ structlog_sentry_logger/__init__.py
@@ -20,21 +20,29 @@ __all__: List[str] = [
     "getLogger",
 ]
 
-# Inject into the environment ONLY the env vars required by the library;
-# we manually update/add to the the environment ONLY the keys in a user's `.env` for
-# which the library is inspecting (i.e., the set intersection between the
-# aforementioned), and only if they weren't already defined in the environment.
-users_dotenv_values = dotenv_values(find_dotenv())
-legal_env_vars_keys = (
-    _ENV_VARS_REQUIRED_BY_LIBRARY.values() & users_dotenv_values.keys()
-)
 
-for k in legal_env_vars_keys:
-    v = users_dotenv_values[k]
-    # Any env-var-to-add already defined in the environment will take precedent over
-    # what is defined in a user's `.env` file.
-    if k not in os.environ and v is not None:
-        os.environ[k] = v
+def _load_library_specific_env_vars() -> None:
+    # Inject into the environment ONLY the env vars required by the library;
+    # we manually update/add to the the environment ONLY the keys in a user's `.env` for
+    # which the library is inspecting (i.e., the set intersection between the
+    # aforementioned), and only if they weren't already defined in the environment.
+    users_dotenv_values = dotenv_values(find_dotenv())
+    legal_env_vars_keys = (
+        _ENV_VARS_REQUIRED_BY_LIBRARY.values() & users_dotenv_values.keys()
+    )
 
-# Note: if DSN isn't defined, will silently not transmit telemetry
-sentry_sdk.init()  # pylint: disable=abstract-class-instantiated
+    for k in legal_env_vars_keys:
+        v = users_dotenv_values[k]
+        # Any env-var-to-add already defined in the environment will take precedent over
+        # what is defined in a user's `.env` file.
+        if k not in os.environ and v is not None:
+            os.environ[k] = v
+
+
+def _init_sentry() -> None:
+    # Note: if DSN isn't defined, will silently not transmit telemetry
+    sentry_sdk.init()  # pylint: disable=abstract-class-instantiated
+
+
+_load_library_specific_env_vars()
+_init_sentry()
