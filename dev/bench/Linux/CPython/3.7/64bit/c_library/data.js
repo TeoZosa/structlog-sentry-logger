6568c493154829b82ad227a35c8dc598ed78c483
github-action-benchmark <github@users.noreply.github.com>
2021-10-14 22:22:10 +0000

    add Benchmark (pytest) benchmark result for f7cc5406ca9c3896cccf9b4efe34187cee73156e

--- dev/bench/Linux/CPython/3.7/64bit/c_library/data.js
+++ dev/bench/Linux/CPython/3.7/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634237125612,
+  "lastUpdate": 1634250130233,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -1430,6 +1430,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0017727134485049814",
             "extra": "mean: 59.59090863157899 msec\nrounds: 19"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "f7cc5406ca9c3896cccf9b4efe34187cee73156e",
+          "message": "Merge pull request #316 from TeoZosa/write-local-logfiles-as-json-lines\n\n💥  Write Local Log Files as JSON Lines Format",
+          "timestamp": "2021-10-14T22:20:07Z",
+          "tree_id": "2f095cebc7ab2a12cd5dede5ab10d7431fa2acc0"
+        },
+        "date": 1634250128821,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 83.33670278103197,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005573902882423466",
+            "extra": "mean: 11.999514819148894 msec\nrounds: 94"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 71.9821767159889,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0018477104971596615",
+            "extra": "mean: 13.892327873684277 msec\nrounds: 95"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 18.818046830222226,
+            "unit": "iter/sec",
+            "range": "stddev: 0.004151228578423579",
+            "extra": "mean: 53.140477809523595 msec\nrounds: 21"
           }
         ]
       }
