f549adcf3fa099044058884f9403f99ac84d08c4
github-action-benchmark <github@users.noreply.github.com>
2021-10-14 05:59:00 +0000

    add Benchmark (pytest) benchmark result for e87f42e723ef448c4ef625e2e00d4ce935d85593

--- dev/bench/Darwin/CPython/3.7/64bit/pure_python/data.js
+++ dev/bench/Darwin/CPython/3.7/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634190777696,
+  "lastUpdate": 1634191139192,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -287,6 +287,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.004426390838431076",
             "extra": "mean: 98.82736363636351 msec\nrounds: 11"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "e87f42e723ef448c4ef625e2e00d4ce935d85593",
+          "message": ":memo: Add `json` to JSON-formatted fenced code blocks",
+          "timestamp": "2021-10-14T05:51:48Z",
+          "tree_id": "0e4a45d936d7ad69c605a49f513b3dbbe07dbb18"
+        },
+        "date": 1634191133757,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 66.85275543247295,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008704428116518167",
+            "extra": "mean: 14.958246575342526 msec\nrounds: 73"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 62.98218108132808,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0012219576271385272",
+            "extra": "mean: 15.877506666666767 msec\nrounds: 75"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 10.249271602902235,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0025033782556596208",
+            "extra": "mean: 97.56790909090896 msec\nrounds: 11"
           }
         ]
       }
