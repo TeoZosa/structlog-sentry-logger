2dcf78b0283a68a5bf2fef92d1a5904b822d3b91
github-action-benchmark <github@users.noreply.github.com>
2021-10-25 17:27:43 +0000

    add Benchmark (pytest) benchmark result for 919230def3b78ffdcea00e1b726bfaba03b006b9

--- dev/bench/Darwin/CPython/3.7/64bit/c_library/data.js
+++ dev/bench/Darwin/CPython/3.7/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635178462215,
+  "lastUpdate": 1635182863387,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -697,6 +697,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0019418550565391656",
             "extra": "mean: 89.62516666666669 msec\nrounds: 12"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "919230def3b78ffdcea00e1b726bfaba03b006b9",
+          "message": "Merge pull request #323 from TeoZosa/feat/use-iso-8601-timestamps\n\n✨  Use ISO 8601 Timestamps For Logs and Filenames",
+          "timestamp": "2021-10-25T17:23:55Z",
+          "tree_id": "9502247c1c55e8c91c2c351bbfbc500657870fa6"
+        },
+        "date": 1635182860879,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 76.54623392529076,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004456321629902392",
+            "extra": "mean: 13.064000000000018 msec\nrounds: 82"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 71.8205187718692,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000670966887736378",
+            "extra": "mean: 13.923597560975596 msec\nrounds: 82"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 11.62592426097877,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0011153905417416498",
+            "extra": "mean: 86.0146666666665 msec\nrounds: 12"
           }
         ]
       }
