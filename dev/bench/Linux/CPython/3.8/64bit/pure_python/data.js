fc657359bbf8424fc6900c76a7ce4cabcc836ef6
github-action-benchmark <github@users.noreply.github.com>
2021-12-01 14:24:04 +0000

    add Benchmark (pytest) benchmark result for dba01235c6d46692aa2ac9afb59d9631d6abc002

--- dev/bench/Linux/CPython/3.8/64bit/pure_python/data.js
+++ dev/bench/Linux/CPython/3.8/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638281874339,
+  "lastUpdate": 1638368644564,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -3029,6 +3029,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0002769058788553494",
             "extra": "mean: 43.37616091304334 msec\nrounds: 23"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "dba01235c6d46692aa2ac9afb59d9631d6abc002",
+          "message": "Merge pull request #354 from TeoZosa/dependabot/pip/sphinxcontrib-confluencebuilder-1.7.1",
+          "timestamp": "2021-12-01T14:14:32Z",
+          "tree_id": "f5d58f6ff20f6a8f8b9511f81e352e145a37bfe5"
+        },
+        "date": 1638368642181,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 106.49956017900453,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00010635240703879605",
+            "extra": "mean: 9.38971013888883 msec\nrounds: 108"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 95.6438059816984,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006952803890701433",
+            "extra": "mean: 10.4554601287129 msec\nrounds: 101"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 20.122541043091868,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003826460508388612",
+            "extra": "mean: 49.69551299999973 msec\nrounds: 20"
           }
         ]
       }
