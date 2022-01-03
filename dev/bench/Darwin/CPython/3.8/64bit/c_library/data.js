a7f9f7dd03270edaa3c5164bd75b59061a942519
github-action-benchmark <github@users.noreply.github.com>
2022-01-03 15:12:47 +0000

    add Benchmark (pytest) benchmark result for deca3932e707eacf37b93abea49a638af757122d

--- dev/bench/Darwin/CPython/3.8/64bit/c_library/data.js
+++ dev/bench/Darwin/CPython/3.8/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640878988393,
+  "lastUpdate": 1641222766783,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -3116,6 +3116,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0023608054063531474",
             "extra": "mean: 84.72799999999985 msec\nrounds: 13"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "deca3932e707eacf37b93abea49a638af757122d",
+          "message": "Merge pull request #385 from TeoZosa/dependabot/pip/pygments-2.11.1",
+          "timestamp": "2022-01-03T14:17:10Z",
+          "tree_id": "3f26ff49051fee3ea98e5b22fb7017b651e4e202"
+        },
+        "date": 1641222759533,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 71.58169516609739,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008633026988822514",
+            "extra": "mean: 13.970051948051953 msec\nrounds: 77"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 66.53415981673629,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008294680619665421",
+            "extra": "mean: 15.029873417721518 msec\nrounds: 79"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 10.794279032112955,
+            "unit": "iter/sec",
+            "range": "stddev: 0.005059478024340682",
+            "extra": "mean: 92.64166666666688 msec\nrounds: 12"
           }
         ]
       }
