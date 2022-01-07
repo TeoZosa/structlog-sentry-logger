0d959a69096c45b046020e6425b9408921ba24cc
github-action-benchmark <github@users.noreply.github.com>
2022-01-07 01:46:15 +0000

    add Benchmark (pytest) benchmark result for 99728b92f1dbd308a3fbbeaf428cc72fa9935911

--- dev/bench/Darwin/CPython/3.7/64bit/c_library/data.js
+++ dev/bench/Darwin/CPython/3.7/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641512249918,
+  "lastUpdate": 1641519975373,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -3444,6 +3444,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.002379568314555743",
             "extra": "mean: 88.31550000000021 msec\nrounds: 12"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "99728b92f1dbd308a3fbbeaf428cc72fa9935911",
+          "message": "Merge pull request #393 from TeoZosa/feat/remove-automatic-sentry-initialization",
+          "timestamp": "2022-01-07T01:41:16Z",
+          "tree_id": "be055794c6fd25fcd54a6a8643510de266c6ea4d"
+        },
+        "date": 1641519973749,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 67.76292013010496,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003995345178904103",
+            "extra": "mean: 14.757333333333301 msec\nrounds: 72"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 63.77805237772546,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007652426189419871",
+            "extra": "mean: 15.679375000000013 msec\nrounds: 72"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 11.43510577472839,
+            "unit": "iter/sec",
+            "range": "stddev: 0.003506054929214276",
+            "extra": "mean: 87.45000000000019 msec\nrounds: 13"
           }
         ]
       }
