1f27eac30a9d87e431f4a1aa52bea040e33d07a6
github-action-benchmark <github@users.noreply.github.com>
2022-01-07 02:56:26 +0000

    add Benchmark (pytest) benchmark result for 0766d7ba31d69b61ed71365c5de6f160fe7d8c6f

--- dev/bench/Linux/CPython/3.7/64bit/c_library/data.js
+++ dev/bench/Linux/CPython/3.7/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641521596200,
+  "lastUpdate": 1641524186077,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -4546,6 +4546,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00011015372346015916",
             "extra": "mean: 45.049722727272695 msec\nrounds: 22"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "0766d7ba31d69b61ed71365c5de6f160fe7d8c6f",
+          "message": "Merge pull request #394 from TeoZosa/template-update/20220106\n\n🍪 Fetch Upstream Project Template Updates (2022-01-06)",
+          "timestamp": "2022-01-07T02:54:38Z",
+          "tree_id": "1c9662cc01e7ce4b31ae7bce52abc23357828489"
+        },
+        "date": 1641524184304,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 102.846596673272,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005565361737162906",
+            "extra": "mean: 9.723219166666718 msec\nrounds: 120"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 94.62797813046114,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007303937298675712",
+            "extra": "mean: 10.567699107142772 msec\nrounds: 112"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 22.457976914322675,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002207955702122704",
+            "extra": "mean: 44.52760833333325 msec\nrounds: 24"
           }
         ]
       }
