cd1f5bfe7a83f2a9d0facd50e2b67300d131197d
github-action-benchmark <github@users.noreply.github.com>
2022-01-08 22:16:53 +0000

    add Benchmark (pytest) benchmark result for cd679fec8ac6339102e7a5e03ebedbfe3b4df298

--- dev/bench/Darwin/CPython/3.8/64bit/pure_python/data.js
+++ dev/bench/Darwin/CPython/3.8/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641570357444,
+  "lastUpdate": 1641680213102,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -3690,6 +3690,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0033177510960235674",
             "extra": "mean: 95.0427857142858 msec\nrounds: 14"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "id": "cd679fec8ac6339102e7a5e03ebedbfe3b4df298",
+          "message": ":recycle: Explicitly specify UTC format\n\nSame as the default value, but now it is explicit to readers.",
+          "timestamp": "2022-01-08T22:06:22Z",
+          "tree_id": "80a163aca105b9357b1d0a1ac28ed2a5867b9cac"
+        },
+        "date": 1641680204923,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 69.60700646963774,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004114546521134451",
+            "extra": "mean: 14.366369863013654 msec\nrounds: 73"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 66.25167354487922,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003500610183230152",
+            "extra": "mean: 15.093958333333195 msec\nrounds: 72"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 11.654207123947925,
+            "unit": "iter/sec",
+            "range": "stddev: 0.003941594674357251",
+            "extra": "mean: 85.80592307692268 msec\nrounds: 13"
           }
         ]
       }
