fbc093f4a00d4f8f8ff273f0698394ca4057d764
Teo Zosa <erinzosa@ucla.edu>
2022-01-10 12:30:51 -0800

    :chart_with_upwards_trend: Benchmark new and old structlog configs
    :warning:
    Resetting logging configs doubled old config benchmarks performance, so
    relative performance dropped down to ~2.04x to ~11.37x.
    :warning:
    
    Benchmarks:
    ```
    --------------------------------------------------------------------------------------------------------------------------- benchmark: 8 tests --------------------------------------------------------------------------------------------------------------------------
    Name (time in us)                                                                      Min                    Max                   Mean                StdDev                 Median                   IQR            Outliers         OPS            Rounds  Iterations
    -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    test_gold_standard_structlog_non_str_keys                                         781.0000 (1.0)       1,909.0000 (1.0)         905.6237 (1.0)         92.1209 (1.0)         891.0000 (1.0)         58.0000 (1.0)        176;67  1,104.2114 (1.0)        1273           1
    test_gold_standard_structlog                                                      879.0000 (1.13)      2,483.0000 (1.30)      1,091.2515 (1.20)       172.3948 (1.87)      1,036.0000 (1.16)       121.0000 (2.09)       150;90    916.3790 (0.83)       1145           1
    test_orjson_serializer_non_str_keys[structlog-specific config (Optimized)]      2,382.0000 (3.05)      5,082.0000 (2.66)      2,890.0731 (3.19)       332.3299 (3.61)      2,804.5000 (3.15)       140.5000 (2.42)        57;68    346.0120 (0.31)        424           1
    test_orjson_serializer[structlog-specific config (Optimized)]                   2,553.0000 (3.27)      3,861.0000 (2.02)      2,957.4235 (3.27)       151.6551 (1.65)      2,946.0000 (3.31)       119.5000 (2.06)        62;34    338.1322 (0.31)        392           1
    test_stdlib_json_serializer[structlog-specific config (Optimized)]              3,348.0000 (4.29)      4,514.0000 (2.36)      3,740.6189 (4.13)        99.1179 (1.08)      3,729.0000 (4.19)        89.5000 (1.54)        35;18    267.3354 (0.24)        307           1
    test_orjson_serializer_non_str_keys[stdlib-based config (Legacy)]               4,811.0000 (6.16)      8,484.0000 (4.44)      5,914.8558 (6.53)       797.7719 (8.66)      5,731.0000 (6.43)     1,062.0000 (18.31)        50;4    169.0658 (0.15)        215           1
    test_orjson_serializer[stdlib-based config (Legacy)]                            5,272.0000 (6.75)      9,275.0000 (4.86)      6,296.1649 (6.95)       751.3838 (8.16)      6,092.0000 (6.84)       899.0000 (15.50)        49;6    158.8268 (0.14)        194           1
    test_stdlib_json_serializer[stdlib-based config (Legacy)]                      40,664.0000 (52.07)    46,254.0000 (24.23)    42,548.2400 (46.98)    1,017.7479 (11.05)    42,324.0000 (47.50)      948.5000 (16.35)         4;1     23.5027 (0.02)         25           1
    -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    ```

--- tests/benchmarks/test_benchmarks.py
+++ tests/benchmarks/test_benchmarks.py
@@ -1,6 +1,8 @@
 from typing import Generator
 
 import pytest
+import structlog
+from _pytest.monkeypatch import MonkeyPatch
 from pytest_benchmark.fixture import BenchmarkFixture
 
 import structlog_sentry_logger
@@ -10,18 +12,55 @@ from tests.benchmarks.utils import TestCases
 TEST_CASES = TestCases()
 
 
-def test_logging_orjson_serializer_non_str_keys(benchmark: BenchmarkFixture) -> None:
+@pytest.mark.usefixtures("temporarily_reset")
+@pytest.mark.parametrize(
+    "is_stdlib_based_structlog_config_requested",
+    [True, False],
+    ids=["stdlib-based config (Legacy)", "structlog-specific config (Optimized)"],
+)
+def test_logging_orjson_serializer_non_str_keys(
+    benchmark: BenchmarkFixture,
+    monkeypatch: MonkeyPatch,
+    is_stdlib_based_structlog_config_requested: bool,
+) -> None:
+    if is_stdlib_based_structlog_config_requested:
+        monkeypatch.setenv("STRUCTLOG_SENTRY_LOGGER_STDLIB_BASED_LOGGER", "ANY_VALUE")
     _benchmark_runner(benchmark, test_cases=TEST_CASES.non_str_keys)
 
 
-def test_logging_orjson_serializer(benchmark: BenchmarkFixture) -> None:
+@pytest.mark.usefixtures("temporarily_reset")
+@pytest.mark.parametrize(
+    "is_stdlib_based_structlog_config_requested",
+    [True, False],
+    ids=["stdlib-based config (Legacy)", "structlog-specific config (Optimized)"],
+)
+def test_logging_orjson_serializer(
+    benchmark: BenchmarkFixture,
+    monkeypatch: MonkeyPatch,
+    is_stdlib_based_structlog_config_requested: bool,
+) -> None:
+    if is_stdlib_based_structlog_config_requested:
+        monkeypatch.setenv("STRUCTLOG_SENTRY_LOGGER_STDLIB_BASED_LOGGER", "ANY_VALUE")
     _benchmark_runner(benchmark, test_cases=TEST_CASES.basic)
 
 
+@pytest.mark.usefixtures("temporarily_reset")
 @pytest.mark.usefixtures("temporarily_set_stlib_json_as_default_serializer")
-def test_logging_stdlib_json_serializer(benchmark: BenchmarkFixture) -> None:
+@pytest.mark.parametrize(
+    "is_stdlib_based_structlog_config_requested",
+    [True, False],
+    ids=["stdlib-based config (Legacy)", "structlog-specific config (Optimized)"],
+)
+def test_logging_stdlib_json_serializer(
+    benchmark: BenchmarkFixture,
+    monkeypatch: MonkeyPatch,
+    is_stdlib_based_structlog_config_requested: bool,
+) -> None:
     test_cases = TEST_CASES.basic.copy()
-    test_cases["uuid"] = str(test_cases["uuid"])
+    if is_stdlib_based_structlog_config_requested:
+        monkeypatch.setenv("STRUCTLOG_SENTRY_LOGGER_STDLIB_BASED_LOGGER", "ANY_VALUE")
+    else:
+        test_cases["uuid"] = str(test_cases["uuid"])
     _benchmark_runner(benchmark, test_cases=test_cases)
 
 
@@ -42,3 +81,21 @@ def temporarily_set_stlib_json_as_default_serializer() -> Generator:
     # Teardown
     structlog_sentry_logger._config._toggle_json_library(use_orjson=True)
     # pylint: enable=protected-access
+
+
+@pytest.fixture(scope="function")
+def temporarily_reset(monkeypatch: MonkeyPatch) -> Generator:
+    # Setup
+    structlog.reset_defaults()
+    # pylint:disable=protected-access
+    structlog_sentry_logger._config._CONFIGS.stdlib_logging_config_already_configured = (
+        False
+    )
+    # pylint:enable=protected-access
+    monkeypatch.delenv("STRUCTLOG_SENTRY_LOGGER_STDLIB_BASED_LOGGER", raising=False)
+
+    # Pass control back to calling function
+    yield
+
+    # Teardown
+    structlog.reset_defaults()
