fe77eebc3abb642af88bdd4aaa3eb8ee6f543723
github-action-benchmark <github@users.noreply.github.com>
2022-01-10 15:07:51 +0000

    add Benchmark (pytest) benchmark result for e1d0a65388fe9048bdf0f94beff3a8aaf1d1116e

--- dev/bench/Linux/CPython/3.9/64bit/pure_python/data.js
+++ dev/bench/Linux/CPython/3.9/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641709583584,
+  "lastUpdate": 1641827271506,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -4792,6 +4792,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00009829938640925942",
             "extra": "mean: 50.294910000000215 msec\nrounds: 20"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "e1d0a65388fe9048bdf0f94beff3a8aaf1d1116e",
+          "message": "Merge pull request #399 from TeoZosa/dependabot/github_actions/pypa/gh-action-pypi-publish-1.5.0",
+          "timestamp": "2022-01-10T14:10:38Z",
+          "tree_id": "d2102bd6ce0fa2f698124dff9002ed7b1c1aa5f8"
+        },
+        "date": 1641827269634,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 68.28760109806471,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0015219891437865123",
+            "extra": "mean: 14.643946835443018 msec\nrounds: 79"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 64.37832894974133,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009329748628713197",
+            "extra": "mean: 15.533177333333345 msec\nrounds: 75"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 18.288886549072622,
+            "unit": "iter/sec",
+            "range": "stddev: 0.004663370159153612",
+            "extra": "mean: 54.67801428571425 msec\nrounds: 21"
           }
         ]
       }
