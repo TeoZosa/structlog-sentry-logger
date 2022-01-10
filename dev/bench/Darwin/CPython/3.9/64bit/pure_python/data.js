5a5c5ff39fe7b0321b3bb436c307ef927fbfc779
github-action-benchmark <github@users.noreply.github.com>
2022-01-10 17:40:41 +0000

    add Benchmark (pytest) benchmark result for 402727a64e3560c9419794a8c99f7b48641eb63c

--- dev/bench/Darwin/CPython/3.9/64bit/pure_python/data.js
+++ dev/bench/Darwin/CPython/3.9/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641835864827,
+  "lastUpdate": 1641836441051,
   "repoUrl": "https://github.com/TeoZosa/structlog-sentry-logger",
   "entries": {
     "Benchmark": [
@@ -3936,6 +3936,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0013366555051920058",
             "extra": "mean: 76.52935714285711 msec\nrounds: 14"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "402727a64e3560c9419794a8c99f7b48641eb63c",
+          "message": "Merge pull request #401 from TeoZosa/dependabot/pip/tox-gh-actions-2.9.0",
+          "timestamp": "2022-01-10T16:43:57Z",
+          "tree_id": "21bcf2c6053e9e6fd892df1f531d826865b9ecc1"
+        },
+        "date": 1641836439216,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer_non_str_keys",
+            "value": 70.53612154784491,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003573980797197919",
+            "extra": "mean: 14.17713333333328 msec\nrounds: 75"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_orjson_serializer",
+            "value": 67.31947861063823,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003825067038904777",
+            "extra": "mean: 14.854541666666654 msec\nrounds: 72"
+          },
+          {
+            "name": "tests/test_benchmarks.py::test_logging_stdlib_json_serializer",
+            "value": 12.979331341933092,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002147197232683406",
+            "extra": "mean: 77.04557142857128 msec\nrounds: 14"
           }
         ]
       }
