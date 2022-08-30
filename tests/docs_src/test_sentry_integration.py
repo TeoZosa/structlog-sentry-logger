e8f58a998a1005a0e99e76a69eea9358b91b3109
Teo Zosa <erinzosa@ucla.edu>
2022-08-30 22:42:50 +0900

    :white_check_mark: Simplify sentry integration test

--- tests/docs_src/test_sentry_integration.py
+++ tests/docs_src/test_sentry_integration.py
@@ -2,15 +2,12 @@ from typing import List
 
 import pytest
 from _pytest.capture import CaptureFixture
-from _pytest.logging import LogCaptureFixture
 from _pytest.monkeypatch import MonkeyPatch
 
-import structlog_sentry_logger
 import tests.docs_src.utils
 import tests.utils
 
 JSONOutputType = tests.utils.JSONOutputType
-_ = structlog_sentry_logger.get_logger()
 
 
 @pytest.fixture(scope="function")
@@ -42,22 +39,12 @@ def expected_output_truncated() -> List[JSONOutputType]:
 
 @pytest.fixture(scope="function")
 def actual_output(
-    capsys: CaptureFixture, caplog: LogCaptureFixture, monkeypatch: MonkeyPatch
+    capsys: CaptureFixture, monkeypatch: MonkeyPatch
 ) -> List[JSONOutputType]:
     tests.utils.enable_sentry_integration_mode(monkeypatch)
     with pytest.raises(RuntimeError):
-        with capsys.disabled():
-            # must import locally to avoid `NameError: name 'sentry_integration' is not defined` will be thrown
-            from docs_src import (  # pylint: disable=import-outside-toplevel
-                sentry_integration,
-            )
-        caplog.clear()
-        # caplog/capsys reset to prevent duplicate entries
-        tests.docs_src.utils.reload_module_non_dev_local_env(
-            monkeypatch, sentry_integration
-        )
-    tests.docs_src.utils.redirect_captured_logs_to_stdout(caplog)
-    return tests.docs_src.utils.get_validated_json_output(capsys)
+        import docs_src.sentry_integration  # pylint: disable=import-outside-toplevel,unused-import
+    return tests.utils.get_validated_json_output(capsys)
 
 
 # pylint: disable=redefined-outer-name
